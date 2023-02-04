5b501f91bd0ef8eb42531b3e5f0972fb00e1990e
DieissonNarde <dieissonnarde@gmail.com>
2023-02-04 02:51:34 -0400

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { Login } from './pages/Login';
+import './styles/global.css';
+
+export default function App(): JSX.Element {
+  return <Login />;
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#edf3e2',
+        principal: '#a9fa00',
+        green: '#00b64c',
+        yellow: '#ffc800',
+        blue: '#345ebc',
+        pink: '#ff9ea3',
+        gray: '#aaaaaa',
+      },
+    },
+  },
+  plugins: [],
+};
