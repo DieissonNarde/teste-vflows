7c778fcc760bdf717e1d4910f27b09785dc3022f
DieissonNarde <dieissonnarde@gmail.com>
2023-02-06 03:48:01 -0400

    feat(Button): add props

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,14 +1,17 @@
 interface ButtonProps {
   text: string;
   bgColor: string;
+  type: string;
+  handleAction?: (ev: React.MouseEvent<HTMLInputElement, MouseEvent>) => void | undefined;
 }
 
-export function Button({ text, bgColor }: ButtonProps) {
+export function Button({ text, bgColor, handleAction, type }: ButtonProps) {
   return (
     <input
       className={`${bgColor} py-4 px-28 mt-8 text-white text-xl font-semibold uppercase hover:brightness-110 cursor-pointer`}
-      type="submit"
+      type={type}
       value={text}
+      // onClick={(ev) => handleAction(ev)}
     />
   );
 }
