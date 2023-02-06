4bdacbac0c173e3b523ea842915e4a697ac97b01
DieissonNarde <dieissonnarde@gmail.com>
2023-02-06 03:53:14 -0400

    feat: implement dynamic table listing

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -1,6 +1,11 @@
 import { HiSearchCircle } from 'react-icons/hi';
+import { IContracts } from '../@types/contracts';
 
-export function Table() {
+interface TableProps {
+  contracts: Array<IContracts>;
+}
+
+export function Table({ contracts }: TableProps) {
   return (
     <table className="w-full">
       <thead className="bg-gray">
@@ -21,28 +26,30 @@ export function Table() {
       </thead>
 
       <tbody className="bg-gray-200 divide-y divide-white">
-        <tr>
-          <td className="px-6 py-4 text-xl font-semibold text-gray-900 text-center whitespace-nowrap">
-            <div className="flex gap-8">
-              <input
-                type="checkbox"
-                className="bg-white p-4 appearance-none checked:bg-blue cursor-pointer"
-                name="select"
-                id="select"
-              />
-              Titulo do contrato
-            </div>
-          </td>
-          <td className="px-6 py-4 text-xl font-semibold text-gray-900 text-center whitespace-nowrap">
-            0820231303
-          </td>
-          <td className="px-6 py-4 bg-blue text-xl font-semibold text-white text-center whitespace-nowrap">
-            5%
-          </td>
-          <td className="text-xl font-semibold text-gray-900  whitespace-nowrap">
-            <HiSearchCircle className="text-5xl mx-auto text-blue" />
-          </td>
-        </tr>
+        {contracts.map((contract) => (
+          <tr key={contract.id}>
+            <td className="px-6 py-4 text-xl font-semibold text-gray-900 text-center whitespace-nowrap">
+              <div className="flex gap-8">
+                <input
+                  type="checkbox"
+                  className="bg-white p-4 appearance-none checked:bg-blue cursor-pointer"
+                  name="select"
+                  id="select"
+                />
+                {contract.nomeDoContrato}
+              </div>
+            </td>
+            <td className="px-6 py-4 text-xl font-semibold text-gray-900 text-center whitespace-nowrap">
+              {contract.codigoDoContrato}
+            </td>
+            <td className="px-6 py-4 bg-blue text-xl font-semibold text-white text-center whitespace-nowrap">
+              {contract.retencaoTecnica}
+            </td>
+            <td className="text-xl font-semibold text-gray-900  whitespace-nowrap">
+              <HiSearchCircle className="text-5xl mx-auto text-blue" />
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
