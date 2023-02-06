f0d46aef531e3d8fbb7f2545ce7763df5c67c349
DieissonNarde <dieissonnarde@gmail.com>
2023-02-06 03:56:08 -0400

    feat: implement login and redirect to another page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,9 +1,35 @@
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../components/Button';
 import { Input } from '../components/Input';
 import { Layout } from '../components/Layout';
 import { Logo } from '../components/Logo';
+import { AuthContext } from '../contexts/AuthContext';
+import api from '../services/api';
 
 export function Login() {
+  const [cnpj, setCnpj] = useState<string>('');
+  const { updateAuth, updateUser } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const response = await api.post('/login', { cnpj });
+
+    console.log('Response: ', response);
+
+    if (response.statusText == 'OK') {
+      updateUser(response.data);
+      updateAuth(true);
+      navigate('/contracts');
+    } else {
+      updateAuth(false);
+      console.error('CNPJ inválido');
+    }
+  }
+
   return (
     <Layout width={'w-2/5'}>
       <div className="flex justify-center mb-12">
@@ -12,9 +38,9 @@ export function Login() {
 
       <p className="mb-6 text-center text-2xl font-semibold uppercase">Pagamento de Fornecedor</p>
       <div className="mx-24 border border-slate-200 p-6">
-        <form className="flex flex-col">
-          <Input width={'w-full'} label={'cnpj'} type={'text'} value={'cnpj'} />
-          <Button text={'Acessar'} bgColor="bg-green" />
+        <form onSubmit={handleLogin} className="flex flex-col">
+          <Input width={'w-full'} label={'cnpj'} type={'text'} value={cnpj} setValue={setCnpj} />
+          <Button type={'submit'} text={'Acessar'} bgColor="bg-green" />
         </form>
       </div>
     </Layout>
