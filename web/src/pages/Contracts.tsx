21fc171ed9a6f68aee6a66531772cd2125dd9fa6
DieissonNarde <dieissonnarde@gmail.com>
2023-02-09 01:35:13 -0400

    feat(Contracts):save contract selected in context

--- web/src/pages/Contracts.tsx
+++ web/src/pages/Contracts.tsx
@@ -1,24 +1,31 @@
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { IContracts } from '../@types/contracts';
+import { IContract } from '../@types/contracts';
 import { Footer } from '../components/Footer';
 import { GroupButtons } from '../components/GroupButtons';
 import { Header } from '../components/Header';
 import { Layout } from '../components/Layout';
 import { Table } from '../components/Table';
 import { AuthContext } from '../contexts/AuthContext';
+import { ContractContext } from '../contexts/ContractContext';
 import api from '../services/api';
 
 export function Contracts() {
-  const [contracts, setContracts] = useState<Array<IContracts>>([]);
-  const [selectedContract, setSelectedContract] = useState<string[]>([]);
+  const [contracts, setContracts] = useState<Array<IContract>>([]);
+  const [selectedContract, setSelectedContract] = useState<Array<string>>([]);
 
   const { user } = useContext(AuthContext);
+  const { updateContract } = useContext(ContractContext);
 
   const navigate = useNavigate();
 
   function handleClick() {
     if (selectedContract.length === 1) {
+      const contract = contracts.find(
+        (contract) => contract.codigoDoContrato === selectedContract[0]
+      );
+      contract && updateContract(contract);
+
       navigate('/invoice');
     } else if (selectedContract.length > 1) {
       alert('Somente um Contrato deverá ser selecionado');
@@ -36,6 +43,7 @@ export function Contracts() {
         setContracts(response.data.contracts);
       } else {
         console.error('CNPJ sem contratos ativos');
+        alert('CNPJ sem contratos ativos');
       }
     }
     getUserContracts();
@@ -49,7 +57,7 @@ export function Contracts() {
         selectedContract={selectedContract}
         setSelectedContract={setSelectedContract}
       />
-      <GroupButtons toLinkBack="/" handleAction={handleClick} />
+      <GroupButtons toLinkBack="/" typeSecondButton="button" handleAction={handleClick} />
       <Footer />
     </Layout>
   );
